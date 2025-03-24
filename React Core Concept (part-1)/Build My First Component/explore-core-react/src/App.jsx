e09d41739cb036616ad5f8ceb11ab178b5121fe5
mdmahfuz307 <njnirob2019@gmail.com>
2025-03-25 00:23:06 +0600

    props

--- React Core Concept (part-1)/Build My First Component/explore-core-react/src/App.jsx
+++ React Core Concept (part-1)/Build My First Component/explore-core-react/src/App.jsx
@@ -7,15 +7,68 @@ function App() {
             <Student></Student>
             <Student></Student>
             <Person></Person>
+            <Developer name="Mozumder"></Developer>
+            <Developer name="Rajon"></Developer>
+            <Developer name="Sabana"></Developer>
+            <Player name="tamim" runs="5000"></Player>
+            <Player name="shakib" runs="4000"></Player>
+            <Salami event="ROja Eid" amount="39"></Salami>
+            <Salami event="Graduation"></Salami>
         </>
     );
 }
 
+
+function Salami({event,amount}) {
+    return (
+        <div className='student'>
+            <p>Salami For :{event}</p>
+            <p>Amount {amount}</p>
+        </div>
+    )
+
+}
+
+
+
+// const j{name,runs} = {name: "Tamim", runs: 5000}
+function Player({name, runs}) {
+    return (
+        <div className='student'>
+            <h3>Player: {name}</h3>
+            <p>Runs: {runs}</p>
+        </div>
+    )
+}
+
+
+
+function Developer(props) {
+    console.log(props);
+    return (
+        <div style={{ border: "2px solid red", margin: "10px" }}>
+            <h3>Developer: {props.name}</h3>
+            <p>Technology: </p>
+        </div>
+    )
+}
+
+
+
 function Person() {
     const age = 17;
     const name = `Nirob`;
+
+    const personStyle = {
+        color: "red",
+        fontSize: "20px",
+        backgroundColor: "yellow",
+        padding: "10px",
+        border: "2px solid green",
+        margin: "10px",
+    };
     return (
-        <p>
+        <p style={personStyle}>
             I am a Person {name} {age}
         </p>
     );
